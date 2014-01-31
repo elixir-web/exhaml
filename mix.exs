b6e6a3527d52de272114aec6d8a0b5d72b0e7aa9
Paulo A Pereira <odesassossegado@gmail.com>
2014-01-31 15:59:11 +0000

    Adds dialyzer options to project. Fixes unused vars warnig.
    Added dialyzer options key in project options.
    Fixed unused vars warnings on compilation.

--- mix.exs
+++ mix.exs
@@ -2,22 +2,22 @@ defmodule Exhaml.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :exhaml,
+    [
+      app: :exhaml,
       version: "0.0.1",
-      elixir: "~> 0.12.1-dev",
-      deps: deps ]
+      #elixir: "~> 0.12.1-dev",
+      deps: deps,
+      dialyzer: [
+             flags: ["-Wunmatched_returns","-Werror_handling","-Wrace_conditions", "-Wno_opaque"],
+             paths: ["_build/shared/lib/exhaml/ebin"]
+           ]
+    ]
   end
 
-  # Configuration for the OTP application
   def application do
     [mod: { Exhaml, [] }]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     []
   end
